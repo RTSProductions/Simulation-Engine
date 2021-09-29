9564e892afda9d739b44295c9caf1dc0aed80df2
Regin Hurley David <reginhurleydavid@MacBook-Air.local>
2021-09-28 18:14:41 -0600

    adding loader class for loading vertices

--- src/Simulation/MainSimulationLoop.java
+++ src/Simulation/MainSimulationLoop.java
@@ -3,7 +3,9 @@ package Simulation;
 import org.lwjgl.opengl.Display;
 
 import RenderEngine.DisplayManager;
-import RenderEngine.MasterRenderer;
+import RenderEngine.Loader;
+import RenderEngine.RawModel;
+import RenderEngine.Renderer;
 
 public class MainSimulationLoop {
 
@@ -11,17 +13,34 @@ public class MainSimulationLoop {
 	{
 		DisplayManager.createDisplay("Simulation");
 		
-		MasterRenderer renderer = new MasterRenderer();
+		Renderer renderer = new Renderer();
+		Loader loader = new Loader();
+		
+		float[] vertices = 
+		{
+				// Left bottom triangle
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				// Right top triangle
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
 		
 		while (!Display.isCloseRequested()) 
 		{
-			
 			renderer.prepare();
 			
-			DisplayManager.updateDisplay();
+			renderer.render(model);
+			
+			DisplayManager.updateDisplay(); 
 			
 		}
 		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
 	}
