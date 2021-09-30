0e16e7d2e45a3933d0be0421f53c1ad40f5396ad
Regin Hurley David <reginhurleydavid@MacBook-Air.local>
2021-09-29 19:21:22 -0600

    adding obj loading

--- src/Simulation/MainSimulationLoop.java
+++ src/Simulation/MainSimulationLoop.java
@@ -9,6 +9,7 @@ import Models.RawModel;
 import Models.TexturedModel;
 import RenderEngine.DisplayManager;
 import RenderEngine.Loader;
+import RenderEngine.OBJLoader;
 import RenderEngine.Renderer;
 
 import Shaders.StaticShader;
@@ -24,94 +25,12 @@ public class MainSimulationLoop {
 		Renderer renderer = new Renderer(shader);
 		Loader loader = new Loader();
 		
-		float[] vertices = {			
-				-0.5f,0.5f,0,	
-				-0.5f,-0.5f,0,	
-				0.5f,-0.5f,0,	
-				0.5f,0.5f,0,		
-				
-				-0.5f,0.5f,1,	
-				-0.5f,-0.5f,1,	
-				0.5f,-0.5f,1,	
-				0.5f,0.5f,1,
-				
-				0.5f,0.5f,0,	
-				0.5f,-0.5f,0,	
-				0.5f,-0.5f,1,	
-				0.5f,0.5f,1,
-				
-				-0.5f,0.5f,0,	
-				-0.5f,-0.5f,0,	
-				-0.5f,-0.5f,1,	
-				-0.5f,0.5f,1,
-				
-				-0.5f,0.5f,1,
-				-0.5f,0.5f,0,
-				0.5f,0.5f,0,
-				0.5f,0.5f,1,
-				
-				-0.5f,-0.5f,1,
-				-0.5f,-0.5f,0,
-				0.5f,-0.5f,0,
-				0.5f,-0.5f,1
-				
-		};
-		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-		
-		
-		RawModel model = loader.loadToVAO(vertices, indices, textureCoords);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("Test_Texture"));
+		RawModel model = OBJLoader.loadObjModel("Pumpkin", loader);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("Pumpkin_Texture"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
 		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -4), 0, 0, 0, 1);
 		
 		Camera camera = new Camera();
-		
-		float amount = -0.1f;
 		
 		while (!Display.isCloseRequested()) 
 		{
@@ -122,7 +41,7 @@ public class MainSimulationLoop {
 			//Render things here
 			renderer.render(entity, shader);
 			camera.move();
-			entity.increaseRotation(1, 1, 1);
+			entity.increaseRotation(0, 1, 0);
 			
 			shader.stop();
 			DisplayManager.updateDisplay(); 
